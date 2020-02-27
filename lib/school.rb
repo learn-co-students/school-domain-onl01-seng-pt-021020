d8bedaacad213299f8560e338e1ead972333304a
Terry Threatt <terry.threatt@gmail.com>
2020-02-27 00:27:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   end 
 
   def add_student(name, grade)
-    if @roster[grade] != nil
+    if @roster.has_key?(grade)
       @roster[grade] << name
     else
       @roster[grade] = [name]
